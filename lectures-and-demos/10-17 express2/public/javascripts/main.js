7e226ae5f945be41d2f87a944ba76347dbfc2179
metaraine <raineorshine@gmail.com>
2013-10-17 17:38:25 -0600

    minor

--- lectures-and-demos/10-17 express2/public/javascripts/main.js
+++ lectures-and-demos/10-17 express2/public/javascripts/main.js
@@ -3,8 +3,8 @@ $(function() {
 	$('.body-button').on('click', function() {
 
 		// first argument: 		url
-		// second argument: 	data
-		// third argument: 		callback
+		// second argument: 	data (which gets sent to server)
+		// third argument: 		callback (called after the server sends a response)
 
 		var data = {
 			bodyPart: $(this).attr('data-part')
