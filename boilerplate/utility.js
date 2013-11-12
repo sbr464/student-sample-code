08aed1f912c39e892e16e5c871a98a256885f6d2
Mark McKelvy <mmckelvy@gmail.com>
2013-11-11 17:05:44 -0700

    Update utility.js

--- boilerplate/utility.js
+++ boilerplate/utility.js
@@ -13,7 +13,7 @@ if (!String.prototype.supplant) {
 function map(items, f) {
 	var output = [];
 	for(var i=0, len=items.length; i<len; i++) {
-		output.push(f(items));
+		output.push(f(items[i]));
 	}
 	return output;
 }
