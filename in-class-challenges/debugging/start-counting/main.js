2d225df318d101f155909c8000ef2a8b01c48904
metaraine <raineorshine@gmail.com>
2014-01-31 09:57:42 -0700

    Add debugging challenge starter code.

---
+++ in-class-challenges/debugging/start-counting/main.js
@@ -0,0 +1,303 @@
+$(function() {
+
+	var countingWords = [
+
+		'one!', 		'two!'].concat([  'three!',   'one!', 'two!', 'three!', 
+			
+		'one!',    'two!', 'three!', 
+
+		  'one!',   'two!', 'three!', 
+
+		'one!',    'two!', 'three!', 
+
+		  'one!',   'two!', 'three!', 'one!']
+		).concat(   [ 'two!', 'three!' ]     ).concat([
+				'one!', 'two!', 			'three!'
+
+		]).concat(   [ 'one! ']     ).concat([
+			
+		'one!',    'two!', 'three!', 
+
+		  'one!',   'two!', 'three!', 
+
+
+		'one!', 'two!',   'three!']    ).concat([  'one!',   'two!', 'three!',
+		'one!',    'two!', 'three!', 
+
+		  'one!',   'two!', 'three!', 'one!']
+		).concat(   [ 'two!', 'three!' ]     ).concat([
+				'one!', 'two!', 			'three!'
+
+		]).concat(   [ 'one! ']     ).concat([
+			
+		'one!',    'two!', 'three!', 
+
+		  'one!',   'two!', 'three!', 
+
+		'one!',    'two!', 'three!', 
+
+		  'one!',   'two!', 'three!', 'one!']
+		).concat(   [ 'two!', 'three!' ]     ).concat([
+				'one!', 'two!', 			'three!'
+
+
+		]).concat(   [ 'one! ']     ).concat([
+			
+		'one!',    'two!', 'three!', 
+
+		  'one!',   'two!', 'three!', 
+
+		'one!',    'two!', 'three!', 
+
+		  'one!',   'two!', 'three!', 'one!']
+		).concat(   [ 'two!', 'three!' ]     ).concat([
+
+				'one!', 'two!', 			'three!'
+
+		]).concat(   [ 'one! ']     ).concat([
+			
+		'one!',    'two!', 'three!', 
+
+		  'one!',   'two!', 'three!', 
+
+		'one!', 'two!',   'three!']    ).concat([  'one!',   'two!', 'three!',
+		'one!',    'two!', 'three!', 
+
+
+		  'one!',   'two!', 'three!', 'one!']
+		).concat(   [ 'two!', 'three!' ]     ).concat([
+				'one!', 'two!', 			'three!'
+
+		]).concat(   [ 'one! ']     ).concat([
+			
+		'one!',    'two!', 'three!', 
+
+
+		  'one!',   'two!', 'three!', 
+
+		'one!', 'two!',   'three!']    ).concat([  'one!',   'two!', 'three!',
+		'one!',    'two!', 'three!', 
+
+		  'one!',   'two!', 'three!', 'one!']
+		).concat(   [ 'two!', 'three!' ]     ).concat([
+				'one!', 'two!', 			'three!'
+
+		]).concat(   [ 'one! ']     ).concat([
+			
+
+		'one!',    'two!', 'three!', 
+
+		  'one!',   'two!', 'three!', 
+
+		'one!', 'two!',   'three!']    ).concat([  'one!',   'two!', 'three!',
+		'one!',    'two!', 'three!', 
+
+
+		  'one!',   'two!', 'three!', 'one!']
+		).concat(   [ 'two!', 'three!' ]     ).concat([
+				'one!', 'two!', 			'three!'
+
+		]).concat(   [ 'one! ']     ).concat([
+			
+		'one!',    'two!', 'three!', 
+
+		  'one!',   'two!', 'three!', 
+
+		'one!', 'two!',   'three!']    ).concat([  'one!',   'two!', 'three!',
+		'one!',    'two!', 'three!', 
+
+
+		  'one!',   'two!', 'three!', 'one!']
+		).concat(   [ 'two!', 'three!' ]     ).concat([
+				'one!', 'two!', 			'three!'
+
+		]).concat(   [ 'one! ']     ).concat([
+			
+
+		  'one!',   'two!', 'three!', 
+
+		'one!', 'two!',   'three!']    ).concat([  'one!',   'two!', 'three!',
+		'one!',    'two!', 'three!', 
+
+		  'one!',   'two!', 'three!', 'one!']
+		).concat(   [ 'two!', 'three!' ]     ).concat([
+				'one!', 'two!', 			'three!'
+
+		]).concat(   [ 'one! ']     ).concat([
+			
+		'one!',    'two!', 'three!', 
+
+
+		  'one!',   'two!', 'three!', 
+
+		'one!', 'two!',   'three!']    ).concat([  'one!',   'two!', 'three!',
+
+		'one!',    'two!', 'three!', 
+
+		  'one!',   'two!', 'three!', 'one!']
+		).concat(   [ 'two!', 'three!' ]     ).concat([
+				'one!', 'two!', 			'three!'
+
+		]).concat(   [ 'one! ']     ).concat([
+			
+		'one!',    'two!', 'three!', 
+
+		  'one!',   'two!', 'three!', 
+
+		'one!', 'two!',   'three!']    ).concat([  'one!',   'two!', 'three!',
+		'one!',    'two!', 'three!', 
+
+		  'one!',   'two!', 'three!', 'one!']
+		).concat(   [ 'two!', 'three!' ]     ).concat([
+
+				'one!', 'two!', 			'three!',
+
+
+		'one!',    'two!', 'three!', 
+
+		  'one!',   'two!', 'three!', 
+
+		'one!', 'two!',   'three!']    ).concat([  'one!',   'two!', 'three!',
+		'one!',    'two!', 'three!', 
+
+		  'one!',   'two!', 'three!', 'one!']
+		).concat(   [ 'two!', 'three!' ]     ).concat([
+				'one!', 'two!', 			'three!'
+
+		]).concat(   [ 'one! ']     ).concat([
+			
+		'one!',    'two!', 'three!', 
+
+		  'one!',   'two!', 'three!', 
+
+		'one!', 'two!',   'three!']    ).concat([  'one!',   'two!', 'three!',
+
+
+		'one!',    'two!', 'three!', 
+
+		  'one!',   'two!', 'three!', 'one!']
+		).concat(   [ 'two!', 'three!' ]     ).concat([
+				'one!', 'two!', 			'three!'
+
+		]).concat(   [ 'one! ']     ).concat([
+			
+		'one!',    'two!', 'three!', 
+
+		  'one!',   'two!', 'three!', 
+
+		'one!', 'two!',   'three!']    ).concat([  'one!',   'two!', 'three!',
+		'one!']   [ 'two!', 'three!', 
+
+		  'one!',   'two!', 'three!', 'one!']
+		).concat(   [ 'two!', 'three!' ]     ).concat([
+				'one!', 'two!', 			'three!'
+
+
+		]).concat(   [ 'one! ']     ).concat([
+
+			
+		'one!',    'two!', 'three!', 
+
+		  'one!',   'two!', 'three!', 
+
+		'one!', 'two!',   'three!']    ).concat([  'one!',   'two!', 'three!',
+		'one!',    'two!', 'three!', 
+
+		  'one!',   'two!', 'three!', 'one!']
+		).concat(   [ 'two!', 'three!' ]     ).concat([
+				'one!', 'two!', 			'three!'
+
+		]).concat(   [ 'one! ']     ).concat([
+			
+		'one!',    'two!', 'three!', 
+
+		  'one!',   'two!', 'three!', 
+
+		'one!',    'two!', 'three!', 
+
+
+		  'one!',   'two!', 'three!', 'one!']
+		).concat(   [ 'two!', 'three!' ]     ).concat([
+				'one!', 'two!', 			'three!'
+
+		]).concat(   [ 'one! ']     ).concat([
+			
+		'one!',    'two!', 'three!', 
+
+		  'one!',   'two!', 'three!', 
+
+		'one!', 'two!',   'three!']    ).concat([  'one!',   'two!', 'three!',
+		'one!',    'two!', 'three!', 
+
+		  'one!',   'two!', 'three!', 'one!']
+		).concat(   [ 'two!', 'three!' ]     ).concat([
+
+				'one!', 'two!', 			'three!'
+
+		]).concat(   [ 'one! ']     ).concat([
+			
+
+
+		  'one!',   'two!', 'three!', 
+
+		'one!', 'two!',   'three!']    ).concat([  'one!',   'two!', 'three!',
+		'one!',    'two!', 'three!', 
+
+		  'one!',   'two!', 'three!', 'one!']
+		).concat(   [ 'two!', 'three!' ]     ).concat([
+				'one!', 'two!', 			'three!'
+
+		]).concat(   [ 'one! ']     ).concat([
+			
+		'one!',    'two!', 'three!', 
+
+		  'one!',   'two!', 'three!', 
+
+		'one!', 'two!',   'three!']    ).concat([  'one!',   'two!', 'three!',
+		'one!',    'two!', 'three!', 
+
+		  'one!',   'two!', 'three!', 'one!']
+		).concat(   [ 'two!', 'three!' ]     ).concat([
+
+				'one!', 'two!', 			'three!'
+
+		]).concat(   [ 'one! ']     ).concat([
+			
+		'one!',    'two!', 'three!', 
+
+		  'one!',   'two!', 'three!', 
+
+		'one!',    'two!', 'three!', 
+
+		  'one!',   'two!', 'three!', 'one!']
+		).concat(   [ 'two!', 'three!' ]     ).concat([
+
+				'one!', 'two!', 			'three!'
+
+		]).concat(   [ 'one! ']     ).concat([
+
+		'one!',    'two!', 'three!', 
+
+		  'one!',   'two!', 'three!', 
+
+
+		'one!', 'two!',   'three!']    ).concat([  'one!',   'two!', 'three!',
+
+		'one!',    'two!', 'three!', 
+
+		  'one!',   'two!', 'three!', 'one!']
+		).concat(   [ 'two!', 'three!' ]     ).concat([
+				'one!', 'two!', 			'three!'
+
+		]).concat(   [ 'one! ']     ).concat([
+
+		'one!', 'two!',   'three!']).concat([  'one!', 'two!',     'three!'
+
+
+	]);
+		
+	$('#mainheading').append(countingWords.length)
+	$('#mainquote').text(countingWords.join(' '));
+
+
+});
