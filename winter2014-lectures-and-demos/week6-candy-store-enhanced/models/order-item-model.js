d70fe4128825a938c6ab4ada7153a1e55af7918e
metaraine <raineorshine@gmail.com>
2014-02-26 12:14:47 -0700

    Replace bootstrapped order with unescaped and stringified order and we can get rid of all the schema toObject transform crap

--- winter2014-lectures-and-demos/week6-candy-store-enhanced/models/order-item-model.js
+++ winter2014-lectures-and-demos/week6-candy-store-enhanced/models/order-item-model.js
@@ -1,17 +1,5 @@
 var mongoose = require('mongoose');
 
-var oldDefaultOptions = mongoose.Schema.prototype.defaultOptions;
-mongoose.Schema.prototype.defaultOptions = function() {
-	var options = oldDefaultOptions();
-	options.toObject = {
-		transform: function (doc, ret, options) {
-		  ret._id = ret._id.toString();
-		  ret.product._id = ret.product._id.toString();
-		}
-	};
-	return options;
-};
-
 var OrderItemSchema = new mongoose.Schema({
 	product: Object,
 	quantity: Number,
