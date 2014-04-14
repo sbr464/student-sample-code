b950cec88b60f9ecd06f4fa82434729aaa98e80e
RefactorU Admin <raine@refactoru.com>
2014-04-14 11:38:17 -0600

    week3-unit-test-first

---
+++ spring2014-lectures-and-demos/week3-unit-test-first/2/bdd.js
@@ -0,0 +1,7 @@
+describe('first', function() {
+	it('should return the first n items in the given array', function() {
+
+		expect(first(['a','b','c'])).toBe(['a','b']);
+
+	})
+})
---
+++ spring2014-lectures-and-demos/week3-unit-test-first/2/main.js
@@ -0,0 +1,7 @@
+var first = function(arr, n) {
+	var output = [];
+	for(var i=0; i<n; i++) {
+		output.push(arr[i]);
+	}
+	return output;
+};
