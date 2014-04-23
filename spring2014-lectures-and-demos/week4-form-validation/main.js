ab5fbc7e20b8c301486c8c5598e8c6597944c4f5
RefactorU Admin <raine@refactoru.com>
2014-04-23 09:30:57 -0600

    Working Version 1

--- spring2014-lectures-and-demos/week4-form-validation/main.js
+++ spring2014-lectures-and-demos/week4-form-validation/main.js
@@ -1,3 +1,41 @@
 $(document).on('ready', function() {
   
+	// EVENT HANDLERS
+	$('#signup-form').on('submit', function() {
+		
+		$('.valid').removeClass('valid');
+		$('.invalid').removeClass('invalid');
+
+		var isValid = true;
+
+		if($('#name').val() === '') {
+			$('#name').closest('.form-group').addClass('invalid');
+			isValid = false;
+		}
+		else {
+			$('#name').closest('.form-group').addClass('valid');
+		}
+
+		if($('#email').val() === '') {
+			$('#email').closest('.form-group').addClass('invalid');
+			isValid = false;
+		}
+		else {
+			$('#email').closest('.form-group').addClass('valid');
+		}
+
+		if(isValid) {
+			var thankyou = $('<div class="mega-announce">Thank you!</div>');
+			$('body').append(thankyou);
+			thankyou.hide();
+			thankyou.fadeIn(2000).delay(2000).fadeOut(function() {
+				$('.valid').removeClass('valid');
+				$('.invalid').removeClass('invalid');
+				$('#signup-form')[0].reset();
+			});
+		}
+
+		return false; // e.preventDefault();
+	});
+
 });
