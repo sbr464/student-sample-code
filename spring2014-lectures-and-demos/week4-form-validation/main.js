ee8ff7558b8b9b6a4e617d1eaf1b6f1efe2d1d5d
metaraine <raineorshine@gmail.com>
2014-04-23 11:21:52 -0600

    week4-form-validation: Add clarifying comments and focus logic.

--- spring2014-lectures-and-demos/week4-form-validation/main.js
+++ spring2014-lectures-and-demos/week4-form-validation/main.js
@@ -1,10 +1,12 @@
 $(document).on('ready', function() {
 
+	/** Resets validation by clearing all validation classes. */
 	var clearValidation = function() {
 		$('.valid').removeClass('valid');
 		$('.invalid').removeClass('invalid');
 	};
 
+	// STEPS FOR REFACTORING
 	// 1. describe the discrete piece of functionality (verb)
 	// 2. define a new function with that name
 	// 3. Replace code with function call. Move code into new function.
@@ -12,16 +14,31 @@ $(document).on('ready', function() {
 	// 5. Parameterize the function with the differences. (The parts that are different, become the parameters)
 	// 6. Call the function with the necessary arguments to reproduce the original functionality.
 	// 7. Fix bugs from variables out of scope, etc
+
+	/** Takes a jQuery object, adds the class valid or invalid depending on whether its empty, and returns true or false. Works for multiple inputs. */
 	var validateInput = function(input) {
 		var allValid = true;
+
 		input.each(function() {
 			var inputValid = $(this).val() !== '';
 			$(this).closest('.form-group').addClass(inputValid ? 'valid' : 'invalid');
+
+			// put cursor in the first input that is invalid
+			// && allValid is needed to have this only work for the *first* input that is invalid
+			if(!inputValid && allValid) {
+				$(this)[0].focus();
+			}
+
+			// Update the allValid flag. If the current input is invalid, allValid will be changed to false
+			// same as: allValid = allValid && inputValid
 			allValid &= inputValid;
 		})
+
+		// use the '!!'' trick to convert allValid to a proper boolean
 		return !!allValid;
 	};
 
+	/** Append a thank you announcement to the body and have it fade out after a couple seconds. When it finishes fading out, clear the validation and reset the form. */
 	var sayThankyou = function() {
 		var thankyou = $('<div class="mega-announce">Thank you!</div>');
 		$('body').append(thankyou);
@@ -32,10 +49,13 @@ $(document).on('ready', function() {
 			.fadeOut(function() {
 				clearValidation();
 				$('#signup-form')[0].reset();
+				$('input')[0].focus();
 			});
 	};
  
 	// EVENT HANDLERS
+
+	/** When the form is submitted, clear the validation, re-check the validation, and say thank you only if everything is valid. */
 	$('#signup-form').on('submit', function() {
 		
 		clearValidation();
@@ -46,7 +66,8 @@ $(document).on('ready', function() {
 			sayThankyou();
 		}
 
-		return false; // e.preventDefault();
+		// prevent the default browser behavior of submitting the form to the server
+		return false;
 	});
 
 });
