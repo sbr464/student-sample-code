490faeb629d978e25415c392f08e1b9dc87efc69
metaraine <raineorshine@gmail.com>
2014-05-22 00:17:30 -0600

    Keep recipes in recipe box sorted.

--- spring2014-lectures-and-demos/week8-smoothie-maker/public/js/lib/collections/recipe-box-collection.js
+++ spring2014-lectures-and-demos/week8-smoothie-maker/public/js/lib/collections/recipe-box-collection.js
@@ -1,6 +1,7 @@
 var RecipeBox = Backbone.Collection.extend({
 	model: Recipe,
 	url: '/recipes',
+	comparator: 'name',
 	toArray: function() {
 	  return this.map(function(recipe) {
 	    return recipe.toArray();
